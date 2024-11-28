29e4458f31a1296d5d6245416f85f5004cb4e4a5
Hugo Merzisen <hugo.merzisen@meteo.fr>
2024-11-28 19:02:55 +0100

    ajout nvim-ufo

--- lua/options.lua
+++ lua/options.lua
@@ -67,4 +67,11 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
+-- config for nvim-ufo (folding)
+
+vim.o.foldcolumn = '1' -- '0' is not bad
+vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+vim.o.foldlevelstart = 99
+vim.o.foldenable = true
+
 -- vim: ts=2 sts=2 sw=2 et
